4fa4d87c83fc0f51182612f476b590a943c5feeb
SageAltair <altairsage@gmail.com>
2026-08-24 16:17:27 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -48,7 +48,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://the-small-voice-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
